5bde6032cdc2a3c4592c6cf74df9a7b40791b842
Svenja Dunger <svenjadunger@web.de>
2024-02-07 18:24:02 +0100

    fetch data arrivalAirport

--- ryanair/src/components/Table.tsx
+++ ryanair/src/components/Table.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Table() {
+  const [flights, setFlights] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        "https://www.ryanair.com/api/views/locate/searchWidget/routes/en/airport/BER"
+      );
+      if (response.ok) {
+        const data = await response.json();
+
+        setFlights(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div>Table</div>
-  )
+    <>
+      <h1>Flights from BER</h1>
+      <div>
+        {flights.map((flight, index) => (
+          <div key={index}>
+            <p>{flight.arrivalAirport.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Table
+export default Table;
