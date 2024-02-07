17bddddbec5177fce794a419c6927cb21a067618
Svenja Dunger <svenjadunger@web.de>
2024-02-07 18:23:07 +0100

    import table component

--- ryanair/src/App.tsx
+++ ryanair/src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
+import Table from "./components/Table";
 
 function App() {
-  
-
   return (
     <>
-
+      <h1>Ryanair API</h1>
+      <Table />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
