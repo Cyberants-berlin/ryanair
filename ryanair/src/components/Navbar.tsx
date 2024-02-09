5bc3cfc0115fde0474dfb0975f0c496830f6651c
RehJunior <samet.30101468@gmail.com>
2024-02-09 13:32:03 +0100

    Navbar angepasst auf städte

--- ryanair/src/components/Navbar.tsx
+++ ryanair/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-"use client";
+import app from "./firebaseConfig";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-import * as React from "react";
 // import Link from "next/link";
 
 import { cn } from "../lib/utils";
@@ -15,50 +17,105 @@ import {
   navigationMenuTriggerStyle,
 } from "../components/ui/navigation-menu";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+interface RouteDocument {
+  arrivalAirport: ArrivalAirport;
+  recent: boolean;
+  seasonal: boolean;
+  operator: string;
+  tags: string[];
+}
 
-export function NavigationMenuDemo() {
+interface ArrivalAirport {
+  code: string;
+  name: string;
+  seoName: string;
+  aliases: string[];
+  base: boolean;
+  city: City;
+  region: Region;
+  country: Country;
+  coordinates: Coordinates;
+  timeZone: string;
+}
+
+interface City {
+  name: string;
+  code: string;
+  macCode?: string; // Optional, as macCode may not be available for all entries
+}
+
+interface Region {
+  name: string;
+  code: string;
+}
+
+interface Country {
+  code: string;
+  iso3code: string;
+  name: string;
+  currency: string;
+  defaultAirportCode: string;
+  schengen: boolean;
+}
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+const NavigationMenuDemo = () => {
+  const [cities, setCities] = useState<ArrivalAirport[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const db = getFirestore(app);
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "routes"));
+        const citiesData: ArrivalAirport[] = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data() as RouteDocument; // Cast to RouteDocument
+          citiesData.push(data.arrivalAirport);
+        });
+        setCities(citiesData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+  >(({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  });
+  ListItem.displayName = "ListItem";
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Your Flights</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
@@ -67,7 +124,6 @@ export function NavigationMenuDemo() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-        
                     <div className="mb-2 mt-4 text-lg font-medium">
                       shadcn/ui
                     </div>
@@ -91,57 +147,31 @@ export function NavigationMenuDemo() {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Choose your City</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
+              {cities.map((city) => (
                 <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
+                  key={city.city.code}
+                  title={city.city.name}
+                  href={city.country.name}
                 >
-                  {component.description}
+                  {city.country.name}
+                  <br />
+                  {city.coordinates.latitude}
                 </ListItem>
               ))}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-         
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Contact
+          </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
-}
+};
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
-
-export default NavigationMenuDemo
+export default NavigationMenuDemo;
