6479d6b481503453283afa54792e30aa10d59672
Svenja Dunger <svenjadunger@web.de>
2024-02-09 14:13:06 +0100

    define class names

--- ryanair/src/components/SkeletonCard.tsx
+++ ryanair/src/components/SkeletonCard.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, CardFooter } from ".ui/card";
+import { Skeleton } from './ui/skeleton';
 
 export default function SkeletonCard() {
   return (
-    <div>SkeletonCard</div>
-  )
+    <Card className="flex flex-col justify-between">
+      <CardHeader className="flex-row gap-4 items-center">
+        <Skeleton className="w-12 h-12 rounded-full" />
+        <Skeleton className="h-6 flex-grow" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="h-4 flex-grow mt-4" />
+        <Skeleton className="h-4 flex-grow mt-4" />
+        <Skeleton className="h-4 w-1/2 mt-4" />
+      </CardContent>
+
+          <CardFooter>
+              <Skeleton className="h-10 w-28"/>
+      </CardFooter>
+    </Card>
+  );
 }
--- ryanair/src/components/loading.tsx
+++ ryanair/src/components/loading.tsx
@@ -4,7 +4,7 @@ import SkeletonCard from './SkeletonCard'
 export default function loading() {
     return (
       <main>
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-4 gap-10 px-10">
           {abefecsfh.split("").map((i) => (
             <SkeletonCard key={i} />
           ))}
