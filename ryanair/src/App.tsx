fd9e9c4fa25e52ca4bcec050d08a93367f980923
Rene Kuhn <renekuhn@posteo.de>
2024-02-10 14:50:18 +0100

    Update routes and layout in App component

--- ryanair/src/App.tsx
+++ ryanair/src/App.tsx
@@ -1,26 +1,40 @@
 // import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import DestinationCitiesCard from "./components/Cards";
 import Navbar from "./components/Navbar";
 import DetailComponent from "./components/Detail";
 import Registration from "./components/Registration";
-import {Login} from "./components/Login";
+import { Login } from "./components/Login";
+import { ReactNode } from "react";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+// Update MainLayout to use the MainLayoutProps type
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => (
+  <>
+    <Navbar />
+    <Outlet /> 
+  </>
+);
+
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<DestinationCitiesCard />} />
-          <Route path="/detail/:city" element={<DetailComponent />} /> 
-          <Route path="/register" element={<Registration/>} />
-          <Route path="/login" element={<Login/>}/>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* Nest routes under MainLayout */}
+        <Route element={<MainLayout children={undefined} />}>
+          <Route index element={<DestinationCitiesCard />} />
+          <Route path="/detail/:city" element={<DetailComponent />} />
           <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
-      </Router>
-      
-    </>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
