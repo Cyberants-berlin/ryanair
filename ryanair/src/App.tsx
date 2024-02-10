ce19439c520c1b863479b49a20dadb2367f989e3
Rene Kuhn <renekuhn@posteo.de>
2024-02-10 12:54:06 +0100

    Add detail component and error boundary and routing

--- ryanair/src/App.tsx
+++ ryanair/src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DestinationCitiesCard from "./components/Cards";
 import Navbar from "./components/Navbar";
+import DetailComponent from "./components/Detail";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<DestinationCitiesCard />} />
-          
+          <Route path="/:city" element={<DetailComponent />} /> {/* Add this line */}
         </Routes>
       </Router>
       <div className="{inter.className}">
---
+++ ryanair/src/components/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface State {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<React.PropsWithChildren<{}>, State> {
+    constructor(props: React.PropsWithChildren<{}>) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: any) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: any, errorInfo: any) {
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children; 
+    }
+}
+
+export default ErrorBoundary;
