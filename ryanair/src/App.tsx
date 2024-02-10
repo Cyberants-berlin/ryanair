efe881c7d7629c7e94b013cc8c12a308c48ead58
Svenja Dunger <svenjadunger@web.de>
2024-02-10 12:09:39 +0100

    remove import react

--- ryanair/src/App.tsx
+++ ryanair/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DestinationCitiesCard from "./components/Cards";
 import Navbar from "./components/Navbar";
--- ryanair/src/components/Navbar.tsx
+++ ryanair/src/components/Navbar.tsx
@@ -68,7 +68,7 @@ export default function Navbar() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-                    <Icons.logo className="h-6 w-6" />
+                    <Icons.logo  />
                     <div className="mb-2 mt-4 text-lg font-medium">
                       shadcn/ui
                     </div>
