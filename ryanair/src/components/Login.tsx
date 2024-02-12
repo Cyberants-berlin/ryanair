3839ec991cedb368c8e66b74502534f5e41f9e8a
Svenja Dunger <svenjadunger@web.de>
2024-02-12 10:09:11 +0100

    change login background

--- ryanair/src/components/Login.tsx
+++ ryanair/src/components/Login.tsx
@@ -9,7 +9,7 @@ import { buttonVariants } from "./ui/button"
 
 export function Login(){
     return (
-        <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           to="/register"
           className={cn(
@@ -21,7 +21,10 @@ export function Login(){
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-red-500">
-            <img src="https://images.unsplash.com/photo-1545132147-d037e6c54cfd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Unsplash Image" />
+            <img
+              src="https://images.unsplash.com/photo-1563580853122-1ea105e0e92a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Unsplash Image"
+            />
           </div>
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
@@ -51,7 +54,6 @@ export function Login(){
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            
             <AuthCardLogin />
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
@@ -73,5 +75,5 @@ export function Login(){
           </div>
         </div>
       </div>
-    )
+    );
 }
