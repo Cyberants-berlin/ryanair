b8248ca822bda29a5cc5126fd3f731d7398a3eb3
Svenja Dunger <svenjadunger@web.de>
2024-02-12 10:51:35 +0100

    add github sign in to Register

--- ryanair/src/components/AuthCardRegister.tsx
+++ ryanair/src/components/AuthCardRegister.tsx
@@ -40,49 +40,69 @@ export default function AuthCardRegister(){
       console.error(error);
     }
   };
+
+  const signInWithGitHub = async () => {
+    const provider = new GithubAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
     return (
-        <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid grid-cols-2 gap-6">
-          <Button variant="outline">
-            <Icons.gitHub  />
-            Github
-          </Button>
-          <Button variant="outline" onClick={signUpWithGoogle}>
-            <Icons.google  />
-            Google
-          </Button>
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid grid-cols-2 gap-6">
+            <Button variant="outline" onClick={signInWithGitHub}>
+              <Icons.gitHub />
+              GitHub
+            </Button>
+            <Button variant="outline" onClick={signUpWithGoogle}>
+              <Icons.google />
+              Google
+            </Button>
           </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" onChange={handleEmailChange} placeholder="m@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" onChange={handlePasswordChange} type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full" onClick={signUpWithEmail} >Create account</Button>
-      </CardFooter>
-    </Card>
-    )
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              onChange={handleEmailChange}
+              placeholder="m@example.com"
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              onChange={handlePasswordChange}
+              type="password"
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" onClick={signUpWithEmail}>
+            Create account
+          </Button>
+        </CardFooter>
+      </Card>
+    );
 }
 
 
