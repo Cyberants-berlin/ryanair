5e21a69673ac89ef5592aafdd11f034ecac76e05
Svenja Dunger <svenjadunger@web.de>
2024-02-12 10:01:18 +0100

    change background image

--- ryanair/src/components/Registration.tsx
+++ ryanair/src/components/Registration.tsx
@@ -9,7 +9,7 @@ import { buttonVariants } from "./ui/button"
 
 export default function Registration(){
     return (
-        <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           to="/login"
           className={cn(
@@ -20,7 +20,12 @@ export default function Registration(){
           Login
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="absolute inset-0 bg-red-500">
+            <img
+              src="https://images.unsplash.com/photo-1545132147-d037e6c54cfd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Unsplash Image"
+            />
+          </div>
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +54,6 @@ export default function Registration(){
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            
             <AuthCardRegister />
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
@@ -71,5 +75,5 @@ export default function Registration(){
           </div>
         </div>
       </div>
-    )
+    );
 }
