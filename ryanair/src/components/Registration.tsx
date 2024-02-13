d892e306647358c5822c92fea06db945e59066ef
Svenja Dunger <svenjadunger@web.de>
2024-02-13 10:15:24 +0100

    remove samets brackets

--- ryanair/src/components/Registration.tsx
+++ ryanair/src/components/Registration.tsx
@@ -72,33 +72,33 @@ export default function Registration() {
         </div>
       </div>
       <div className="lg:p-8">
-        {" "}
+        
         <div className="mx-auto  flex  w-full  flex-col  justify-center  space-y-6  sm:w-[350px]">
-          {" "}
-          <AuthCardRegister />{" "}
+          
+          <AuthCardRegister />
           <p className="px-8  text-center  text-sm  text-muted-foreground">
-            {" "}
-            By clicking continue, you agree to our {"  "}
+            
+            By clicking continue, you agree to our 
             <Link
               to="/terms"
               className="underline  underline-offset-4  hover:text-primary"
             >
-              {" "}
-              Terms of Service{" "}
-            </Link>{" "}
-            {"  "}
-            and {"  "}
+              
+              Terms of Service
+            </Link>
+            
+            and 
             <Link
               to="/pivacy"
               className="underline  underline-offset-4  hover:text-primary"
             >
-              {" "}
-              Privacy Policy{" "}
-            </Link>{" "}
-            .{" "}
-          </p>{" "}
-        </div>{" "}
-      </div>{" "}
+              
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
