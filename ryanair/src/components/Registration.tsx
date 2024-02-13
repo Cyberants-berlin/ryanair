d5964c0d0088f9d54adda402bac634651447a6fb
Svenja Dunger <svenjadunger@web.de>
2024-02-13 10:17:15 +0100

    add important brackets

--- ryanair/src/components/Registration.tsx
+++ ryanair/src/components/Registration.tsx
@@ -72,27 +72,22 @@ export default function Registration() {
         </div>
       </div>
       <div className="lg:p-8">
-        
         <div className="mx-auto  flex  w-full  flex-col  justify-center  space-y-6  sm:w-[350px]">
-          
           <AuthCardRegister />
           <p className="px-8  text-center  text-sm  text-muted-foreground">
-            
-            By clicking continue, you agree to our 
+            By clicking continue, you agree to our{"  "}
             <Link
               to="/terms"
               className="underline  underline-offset-4  hover:text-primary"
             >
-              
               Terms of Service
             </Link>
-            
-            and 
+            {"  "}
+            and{"  "}
             <Link
-              to="/pivacy"
+              to="/privacy"
               className="underline  underline-offset-4  hover:text-primary"
             >
-              
               Privacy Policy
             </Link>
             .
