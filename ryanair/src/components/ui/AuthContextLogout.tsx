aa535ae2ddf83220a940df3bd6cd6c0797278c7f
Svenja Dunger <svenjadunger@web.de>
2024-02-14 09:21:48 +0100

    logout button renamed

--- ryanair/src/components/ui/AuthContextLogout.tsx
+++ ryanair/src/components/ui/AuthContextLogout.tsx
@@ -3,5 +3,5 @@ import { useAuth } from "./components/AuthContext";
 const LogoutButton = () => {
   const { logout } = useAuth();
 
-  return <button onClick={logout}>Abmelden</button>;
+  return <button onClick={logout}>Logout</button>;
 };
