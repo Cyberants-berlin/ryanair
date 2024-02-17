550e188c9b82526d4b7fe736bd429789dafd348d
Svenja Dunger <svenjadunger@web.de>
2024-02-17 13:55:50 +0100

    make changes authcontext

--- ryanair/src/components/AuthContext.tsx
+++ ryanair/src/components/AuthContext.tsx
@@ -1,26 +1,18 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  ReactNode,
-} from "react";
-import { auth } from "../firebaseConfig";
+// AuthContext.tsx
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
-
+import { auth } from "../firebaseConfig"; // Adjust the path as necessary
 
 interface AuthContextType {
   currentUser: User | null;
 }
 
-
 const AuthContext = createContext<AuthContextType>({ currentUser: null });
 
 export const useAuth = () => useContext(AuthContext);
 
-
 interface AuthProviderProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
@@ -30,7 +22,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
     });
-
 
     return unsubscribe;
   }, []);
