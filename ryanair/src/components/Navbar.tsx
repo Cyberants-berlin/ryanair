3c570eb7f2b04364fdf2288922087aedc6e1822f
Svenja Dunger <svenjadunger@web.de>
2024-02-17 20:32:31 +0100

    style logged in message

--- ryanair/src/components/Navbar.tsx
+++ ryanair/src/components/Navbar.tsx
@@ -60,79 +60,81 @@ export default function Navbar() {
   const { currentUser } = useAuth();
   return (
     <nav>
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid  gap-3  p-4  md:w-[400px]  lg:w-[500px]  lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex  h-full  w-full  select-none  flex-col  justify-end  rounded-md  bg-gradient-to-b  from-muted/50  to-muted  p-6  no-underline  outline-none  focus:shadow-md"
-                    href="/"
-                  >
-                    <Icons.logo />
-                    <div className="mb-2  mt-4  text-lg  font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm  leading-tight  text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Countries</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid  w-[400px]  gap-3  p-4  md:w-[500px]  md:grid-cols-2  lg:w-[600px]  ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid  gap-3  p-4  md:w-[400px]  lg:w-[500px]  lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <a
+                      className="flex  h-full  w-full  select-none  flex-col  justify-end  rounded-md  bg-gradient-to-b  from-muted/50  to-muted  p-6  no-underline  outline-none  focus:shadow-md"
+                      href="/"
+                    >
+                      <Icons.logo />
+                      <div className="mb-2  mt-4  text-lg  font-medium">
+                        shadcn/ui
+                      </div>
+                      <p className="text-sm  leading-tight  text-muted-foreground">
+                        Beautifully designed components built with Radix UI and
+                        Tailwind CSS.
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href="/docs" title="Introduction">
+                  Re-usable components built using Radix UI and Tailwind CSS.
                 </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link to="/login">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Login
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          {" "}
-          {/* Neuer Chatroom-Link */}
-          <Link to="/chatroom">
+                <ListItem href="/docs/installation" title="Installation">
+                  How to install dependencies and structure your app.
+                </ListItem>
+                <ListItem href="/docs/primitives/typography" title="Typography">
+                  Styles for headings, paragraphs, lists...etc
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Countries</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid  w-[400px]  gap-3  p-4  md:w-[500px]  md:grid-cols-2  lg:w-[600px]  ">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/login">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Login
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             {" "}
-            {/* Verwendung von Link für clientseitige Navigation */}
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Chatroom
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-    { currentUser ? <p>Logged in as {currentUser.email}</p> : <p>Not logged in</p> }
-  </nav>
+            {/* Neuer Chatroom-Link */}
+            <Link to="/chatroom">
+              {" "}
+              {/* Verwendung von Link für clientseitige Navigation */}
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Chatroom
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="text-sm font-bold text white bg-blue-600 p-2 rounded">
+        {currentUser ? `Logged in as ${currentUser.email}` : "Not logged in"}
+      </div>
+    </nav>
   );
 }
 
