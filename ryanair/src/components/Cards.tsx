e87ac9ab03c2ca82d950aaaa8d93896c99b81ac0
Svenja Dunger <svenjadunger@web.de>
2024-02-21 13:13:11 +0100

    add header import

--- ryanair/src/components/Cards.tsx
+++ ryanair/src/components/Cards.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import SkeletonCard from "./SkeletonCard";
 import ErrorBoundary from "./ErrorBoundary";
 import "../main.css";
-import "../Header.css";
+import "./Header.css";
 import Header from "../components/Header";
 
 import {
