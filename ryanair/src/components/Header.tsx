fd74a9c4dca8ae6520493de0c3a3abdc226d88b5
Svenja Dunger <svenjadunger@web.de>
2024-02-21 11:57:26 +0100

    add Header Homepage

---
+++ ryanair/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"; 
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Fly Smart, Save Big: Perfect Weekends with Ryanair!</h1>
+    </div>
+  );
+};
+
+export default Header;
