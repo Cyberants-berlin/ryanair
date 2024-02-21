f8ef7e33fbf69ebfd60f7d28f3042ac7c80bda86
René Kuhn <rene@fedora.fedora.fritz.box>
2024-02-21 14:54:50 +0100

    refactoring header card

--- ryanair/src/components/Header.tsx
+++ ryanair/src/components/Header.tsx
@@ -2,8 +2,8 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1>Fly Smart, Save Big: Perfect Weekends with Ryanair!</h1>
+    <div className="bg-gradient-to-r from-blue-800 to-blue-500 text-white p-5 text-center ">
+      <h1 className="m-0 text-lg font-normal md:text-3xl">Fly Smart, Save Big: Perfect Weekends with Ryanair!</h1>
     </div>
   );
 };
--- ryanair/src/components/Login.tsx
+++ ryanair/src/components/Login.tsx
@@ -27,7 +27,8 @@ export function Login() {
   };
 
   return (
-    <div className="container relative hidden h-[1000px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative hidden h-screen overflow-hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+
       <Link
         to="/register"
         className={cn(
