79c20c5cd88d5436a21c7bfbfb74c70f7cbe3884
Svenja Dunger <svenjadunger@web.de>
2024-02-27 09:09:29 +0100

    add comments usercontext

--- ryanair/src/UserContext.tsx
+++ ryanair/src/UserContext.tsx
@@ -12,20 +12,24 @@ import {
   User as FirebaseUser,
 } from "firebase/auth";
 
+//Def.Typschnittstelle mit Benutzerdaten und Funktionen für Login &Logout
 interface UserContextType {
   user: FirebaseUser | null;
   login: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
 }
 
+//Typschnittstelle f. Eigenschaften des UserProvider
 interface UserProviderProps {
   children: ReactNode;
 }
-
+//Erstellt Kontext f Benutzerdaten mit  undefinierten Standardwert
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const useUser = () => {
+  // benutzt  benutzercontext innerhalb hook
   const context = useContext(UserContext);
+  //Überprüft ob Kontext außerhalb eines UserProvider verw. wird
   if (context === undefined) {
     throw new Error("useUser must be used within a UserProvider");
   }
@@ -33,22 +37,27 @@ export const useUser = () => {
 };
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  //def zustand f. benutzerdaten
   const [user, setUser] = useState<FirebaseUser | null>(null);
-
+  //wird bei erster renderung der kompon. ausgeführt
   useEffect(() => {
+    //regis. listener f. änderungen in authentif.status
     const unsubscribe = auth.onAuthStateChanged(setUser);
     return () => unsubscribe(); // Clean up the subscription
   }, []);
-
+  // Def. e. asynchrone Login-Funktion
   const login = async (email: string, password: string) => {
+    //Führt d. Anmeldung mit E-Mail & Passwort durch
     await signInWithEmailAndPassword(auth, email, password);
   };
-
+  // Def. e. asynchrone Logout-Funktion
   const logout = async () => {
+    //führt abmeldung durch
     await auth.signOut();
   };
 
   return (
+    //Stellt Kontext mit Benutzerdaten und Funktionen bereit
     <UserContext.Provider value={{ user, login, logout }}>
       {children}
     </UserContext.Provider>
